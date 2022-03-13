ce01540ed10ab14301a0e47b642f0a8b0bc7e2c7
MarcoEsquivel1 <marco.esquivel@catolica.edu.sv>
2022-03-12 18:15:57 -0600

    modelos 1

---
+++ models/Mesas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PARCIAL1D.models
+{
+    public class Mesas
+    {
+        [Key]
+        public int MesaId { get; set; }
+        public int EmpresaId { get; set; }
+        public String DescripcionMesa { get; set; }
+        public String ZonaMesa { get; set; }
+        public int SillasMesa { get; set; }
+        public String Estado { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaModificacion { get; set; }
+    }
+}
---
+++ ordenContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PARCIAL1D
+{
+    public class ordenContext : DbContext
+    {
+
+        public ordenContext(DbContextOptions<ordenContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
