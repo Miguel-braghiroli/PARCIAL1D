034151f0b1d41bfbd78d413535bf65e0ad66f5d6
MarcoEsquivel1 <marco.esquivel@catolica.edu.sv>
2022-03-12 18:43:06 -0600

    context1

--- ordenContext.cs
+++ ordenContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PARCIAL1D.models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,12 @@ namespace PARCIAL1D
 
         }
 
-
+        public DbSet<DetalleOrdenes> DetalleOrdenes { get; set; }
+        public DbSet<Empresas> Empresas { get; set; }
+        public DbSet<EncabezadoOrden> EncabezadoOrden { get; set; }
+        public DbSet<Mesas> Mesas { get; set; }
+        public DbSet<Pagos> Pagos { get; set; }
+        public DbSet<Platos> Platos { get; set; }
+        public DbSet<Usuarios> Usuarios { get; set; }
     }
 }
